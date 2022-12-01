1b315ae3537b747c91b311f3c81e0fba6c099fd4
devsunset <devsunset@gmail.com>
2022-12-01 14:42:33 +0900

    dev

--- msa/user-service/src/main/java/com/example/userservice/domain/User.java
+++ msa/user-service/src/main/java/com/example/userservice/domain/User.java
@@ -19,4 +19,14 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String username;
+	
+	public User() {
+		
+	}
+	
+	public User(Long id, String username) {
+		this.id = id;
+		this.username = username;
+		
+	}
 }
